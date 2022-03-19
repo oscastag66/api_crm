ed94e3bfe1b83ee2ca3ddb517e05454991e1e601
unknown <oscastag66@hotmail.com>
2022-03-19 20:55:53 +0100

    first commit

---
+++ src/layout/IniciarSesion.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom'
+
+const IniciarSesion = () => {
+  return (
+    <div>
+      <p>Desde IniciarSesion.jsx</p>
+      <Outlet />
+    </div>
+  )
+}
+
+export default IniciarSesion
